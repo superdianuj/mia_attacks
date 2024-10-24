e19dbfbd014dd0f10053dce882c88216495e366e
Spider Man <dianujkotov15@gmail.com>
2024-10-24 23:42:41 +0900

    Update optimal.py

--- optimal/optimal.py
+++ optimal/optimal.py
@@ -9,8 +9,7 @@ from scipy.stats import norm
 from sklearn.metrics import roc_curve, auc, accuracy_score
 from torch.utils.data import DataLoader, SubsetRandomSampler, TensorDataset, ConcatDataset
 import math
-import matplotlib.pyplot as plt 
-from optimal.shadow import *
+import matplotlib.pyplot as plt
 
 
 
@@ -29,67 +28,46 @@ def calculate_accuracy(model, dataloader, device='cpu'):
 
 
 
-# Function to train a model
-def train_model_with_raw_tensors(model, train_data, train_labels, epochs=100, lr=0.01,bs=128*2, device='cuda'):
-    dataset= torch.utils.data.TensorDataset(train_data, train_labels)
-    train_loader = torch.utils.data.DataLoader(dataset, batch_size=bs, shuffle=True)
-    criterion = nn.CrossEntropyLoss()
-    model.train()
-    optimizer = optim.SGD(model.parameters(), lr=lr)
-    for epoch in range(epochs):
-        for batch in train_loader:
-            img, label = batch
-            img, label = img.to(device), label.to(device)
-            optimizer.zero_grad()
-            outputs = model(img)
-            loss = criterion(outputs, label)
-            loss.backward()
-            optimizer.step()
-    return model
-
-
-def train_model_with_loader(model, train_loader, training_epochs=100, lr=0.01, device='cuda'):
-    criterion = nn.CrossEntropyLoss()
-    optimizer=torch.optim.Adam(model.parameters(), lr=lr)
-    for epochy in range(training_epochs):
-        for batch in train_loader:
-            data, target = batch[0].to(device), batch[1].to(device)
-            output = model(data) 
-            loss = criterion(output, target)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-    return model
-
-
-
-def get_losses(model, dataloader, device):
-    losses=[]
+def get_probability_vectors(model, dataloader, device):
+    prob_vectors = []
     model.eval()
     with torch.no_grad():
         for images, labels in dataloader:
             images = images.to(device)
-            outputs = model.feature(images)
-            los=nn.CrossEntropyLoss(reduction='none')(outputs, labels.to(device))
-            losses.append(los.cpu())
-    return torch.cat(losses).unsqueeze(1)
-
-
+            outputs = model(images)
+            # Get full probability distribution
+            prob_dist = F.softmax(outputs, dim=1)
+            prob_vectors.append(prob_dist.cpu())
+    return torch.cat(prob_vectors, dim=0)
 
 def shadow_zone(target_model, train_loader, test_loader, device='cuda'):
-    in_losses = get_losses(target_model, train_loader, device)
-    out_losses = get_losses(target_model, test_loader, device)
-    return in_losses, out_losses
+    in_probs = get_probability_vectors(target_model, train_loader, device)
+    out_probs = get_probability_vectors(target_model, test_loader, device)
+    return in_probs, out_probs
 
-
-def attack_zone(target_data_col, target_label_col, target_model, in_losses, out_losses, attack_hidden_size, attack_epochs, attack_lr, device='cuda'):
-    data=torch.cat([in_losses,out_losses])
-    labels=torch.tensor([1]*len(in_losses)+[0]*len(out_losses)).unsqueeze(1).to(device)
-    dataset=TensorDataset(data, labels)
-    train_loader=DataLoader(dataset, batch_size=128, shuffle=True)
-    classifier=nn.Sequential(nn.Linear(1,attack_hidden_size),nn.ReLU(),nn.Linear(attack_hidden_size,1),nn.Sigmoid()).to(device) 
+def attack_zone(target_data_col, target_label_col, target_model, in_probs, out_probs, attack_hidden_size, attack_epochs, attack_lr, device='cuda'):
+    # Combine probability vectors from training and test sets
+    data = torch.cat([in_probs, out_probs])
+    # Create labels: 1 for training set (member), 0 for test set (non-member)
+    labels = torch.tensor([1]*len(in_probs)+[0]*len(out_probs)).unsqueeze(1).to(device)
+    
+    dataset = TensorDataset(data, labels)
+    train_loader = DataLoader(dataset, batch_size=128, shuffle=True)
+    
+    num_classes = in_probs.shape[1]  # Get number of classes from probability vector size
+    
+    # Modified attack model to accept full probability vector
+    classifier = nn.Sequential(
+        nn.Linear(num_classes, attack_hidden_size),
+        nn.ReLU(),
+        nn.Linear(attack_hidden_size, 1),
+        nn.Sigmoid()
+    ).to(device)
+    
     criterion = nn.BCELoss()
     optimizer = optim.Adam(classifier.parameters(), lr=attack_lr)
+    
+    # Train the attack model
     classifier.train()
     for epoch in range(attack_epochs):
         run_loss = 0.0
@@ -103,23 +81,23 @@ def attack_zone(target_data_col, target_label_col, target_model, in_losses, out_
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-
+            
             run_loss += loss.item()
+            
         if epoch % 10 == 0 or epoch == attack_epochs - 1 or epoch == 0:
             print(f'Epoch {epoch+1}/{attack_epochs}, Attack Loss: {run_loss/len(train_loader):.4f}')
-
-    scores=[]
+    
+    # Get attack predictions for target data
+    scores = []
     for i in range(len(target_data_col)):
-        target_data=target_data_col[i]
-        target_label=target_label_col[i]
-        target_loss=nn.CrossEntropyLoss()(target_model(target_data.unsqueeze(0).to(device)), target_label.unsqueeze(0).to(device)).unsqueeze(0).detach()
-        scores.append(classifier(target_loss).item())
-                  
+        target_data = target_data_col[i]
+        target_output = target_model(target_data.unsqueeze(0).to(device))
+        prob_vector = F.softmax(target_output, dim=1)
+        scores.append(classifier(prob_vector).item())
+    
     return np.array(scores)
 
-
-
 def MIA(target_model, train_loader, test_loader, target_data_col, target_label_col, attack_hidden_size, attack_epochs, attack_lr, device='cuda'):
-    in_losses, out_losses = shadow_zone(target_model, train_loader, test_loader, device)
-    scores = attack_zone(target_data_col, target_label_col, target_model, in_losses, out_losses, attack_hidden_size, attack_epochs, attack_lr, device)
+    in_probs, out_probs = shadow_zone(target_model, train_loader, test_loader, device)
+    scores = attack_zone(target_data_col, target_label_col, target_model, in_probs, out_probs, attack_hidden_size, attack_epochs, attack_lr, device)
     return scores
