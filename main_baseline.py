155f738f6d12f0828898cff5864381f69a0ee047
Spider Man <dianujkotov15@gmail.com>
2024-10-27 05:04:24 +0900

    Update main_baseline.py

--- main_baseline.py
+++ main_baseline.py
@@ -38,9 +38,9 @@ epochs = 50
 lr = 1e-3
 perc=0.0   # amount of actual training data available to the attacker
 perc_test=0.20    # amount of testing data available to the attacker ( similar distribution to training data)
-meausurement_number=30 
+meausurement_number=10 
 attack_lr=1e-1
-attack_epochs=20
+attack_epochs=30
 attack_hidden_size=8
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #-----------------------------------------------------------------------------------
